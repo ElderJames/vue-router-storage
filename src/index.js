4527633a0d8022e97f1657a2d06c8f0e777360ef
杨舜杰 <shunjiey@hotmail.com>
2017-04-15 00:55:42 +0800

    解决当replace操作出现url相同的情况时所导致的history已执行但路由钩子不执行的问题

--- src/index.js
+++ src/index.js
@@ -82,12 +82,45 @@ RouterStorage.install = function (Vue, option) {
             _history.beforeState = history.state;
             _history.length = history.length;
 
+            var _routeActived = false;
+            var _isRoot = false;
+
+            let goBack = () => {
+                //普通后退处理
+                if (process.env.NODE_ENV == 'development')
+                    console.log('go back')
+                vm.$emit('goback')
+                //后退
+                if (_history.routes.length > 0)
+                    _history.forwardRoutes.push(_history.routes.pop());
+                console.log(_history.forwardRoutes)
+            }
+
+            let replace = () => {
+                vm.$emit('replace')
+                if (process.env.NODE_ENV == 'development')
+                    console.log('router replace :' + vm.$route.fullPath)
+                _history.routes.pop();
+                _history.routes.push(vm.$route.fullPath);
+            }
+
+            let goForward = () => {
+                if (process.env.NODE_ENV == 'development')
+                    console.log('go forward')
+                vm.$emit('goforward')
+                //前进
+                _history.routes.push(vm.$route.fullPath);
+                _history.forwardRoutes = [];
+            }
+
             vm.$router.beforeEach((to, from, next) => {
+                _routeActived = true;
+                _isRoot = false;
                 //在Vue根目录再后退的处理
                 if (to.path == '/__root' && history.state && history.state.key === -1) {
                     if (process.env.NODE_ENV == 'development')
-                        console.log('Is root,can\'t back!')
-                    
+                        console.log('It\'s root,can\'t back!')
+                    _isRoot = true;
                     next(false);
                     _history.beforeState = { key: genKey() }
                     for (var idx = _history.forwardRoutes.length - 1; idx >= 0; idx--) {
@@ -98,48 +131,51 @@ RouterStorage.install = function (Vue, option) {
                 else {
                     next();
 
-                    if (process.env.NODE_ENV == 'development') {
-                        console.log('befaultState:' + JSON.stringify(_history.beforeState));
-                        console.log('currentState:' + JSON.stringify(history.state));
-                    }
                     if (to.path == '/'
                         || history.state
                         && _history.beforeState
                         && history.state.key
                         && Number(_history.beforeState.key) > Number(history.state.key)) {
-
-                        //普通后退处理
-                        if (process.env.NODE_ENV == 'development')
-                            console.log('go back')
-                        vm.$emit('goback')
-                        //后退
-                        if (_history.routes.length > 0)
-                            _history.forwardRoutes.push(_history.routes.pop());
-                        console.log(_history.forwardRoutes)
+                        goBack();
                     }
                     else {
                         //replace处理
                         if (_history.beforeState.key == history.state.key) {
-                            vm.$emit('replace')
-                            if (process.env.NODE_ENV == 'development')
-                                console.log('router replace :' + to.fullPath)
-                            _history.routes.pop();
-                            _history.routes.push(to.fullPath);
+                            replace();
                         }
                         //普通前进处理
                         else {
-                            if (process.env.NODE_ENV == 'development')
-                                console.log('go forward')
-                            vm.$emit('goforward')
-                            //前进
-                            _history.routes.push(to.fullPath);
-                            _history.forwardRoutes = [];
+                            goForward();
                         }
                     }
-
                     _history.beforeState = history.state;
                 }
                 Store.Save()
+            })
+
+
+            window.onpopstate = function (e) {
+                if (_routeActived) {
+                    _routeActived = false;
+                    return;
+                }
+                if (!_isRoot) {
+                    if (Number(_history.beforeState.key) > Number(e.state.key)) {
+                        console.log('补偿后退');
+                        goBack();
+                    }
+                    else if (Number(_history.beforeState.key) < Number(e.state.key)) {
+                        console.log('补偿前进');
+                        goForward();
+                    }
+                    _history.beforeState = history.state;
+                    Store.Save()
+                }
+            }
+
+
+            vm.$router.afterEach(route => {
+                _routeActived = false;
             })
         }
     })
